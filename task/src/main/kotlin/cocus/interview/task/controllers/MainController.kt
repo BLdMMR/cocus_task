c8ae9487ef5b8e6636fadbb133b8457e290eade8
Bernardo Mira Rodrigues <A44784@alunos.isel.pt>
2022-11-29 15:10:49 +0000

    Project Purpose Done. Refactoring and Cleanup needed

--- task/src/main/kotlin/cocus/interview/task/controllers/MainController.kt
+++ task/src/main/kotlin/cocus/interview/task/controllers/MainController.kt
@@ -6,10 +6,12 @@ import cocus.interview.task.UnknownErrorException
 import cocus.interview.task.UnknownUsernameException
 import cocus.interview.task.services.MainServices
 import cocus.interview.task.structures.*
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,13 +24,12 @@ import org.springframework.web.bind.annotation.RestController
 class MainController (private val services: MainServices) {
 
     @GetMapping("/{username}")
-    fun getAllUserRepositories(@PathVariable username: String) : ResponseEntity<OutputEntity>? {
+    fun getAllUserRepositories(@PathVariable username: String, @RequestHeader(HttpHeaders.ACCEPT) accept: String) : ResponseEntity<OutputEntity>? {
         return try {
-
             val repositoryList = services.getAllUserRepositories(username)
             val ret = GitHubRepositoryList(repositoryList!!)
             println("\n\n$ret")
-            return ResponseEntity.ok().body(GitHubRepositoryList(repositoryList!!))
+            return ResponseEntity.ok().body(GitHubRepositoryList(repositoryList))
 
         } catch (gse: GitHubServerException) {
 
--- task/src/test/kotlin/cocus/interview/task/TaskApplicationTests.kt
+++ task/src/test/kotlin/cocus/interview/task/TaskApplicationTests.kt
@@ -1,13 +1,71 @@
 package cocus.interview.task
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MockMvcBuilder
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+
 
 @SpringBootTest
-class TaskApplicationTests {
+class TaskApplicationTests(
+	@Autowired
+	val ctx: WebApplicationContext
+) {
 
 	@Test
 	fun contextLoads() {
 	}
+
+	lateinit var mock :MockMvc
+
+	@BeforeEach
+	fun configureMockMVC() {
+		mock = MockMvcBuilders.webAppContextSetup(ctx).build()
+	}
+
+	@Test
+	fun getAllUserRepositories_OkRequest() {
+		mock.perform(get("/palbp").header("Accept", "application/json"))
+			.andDo(print())
+			.andExpect(status().isOk)
+			.andExpect(content().json("{\"githubRepoList\":[{\"name\":\"laboratory\",\"owner\":{\"login\":\"palbp\"},\"branches\":[{\"name\":\"main\",\"commit\":{\"sha\":\"ce28b2763f80e77fc7bd2086c5f7a7c1a8de6876\"}}],\"fork\":false},{\"name\":\"sempre_a_codar\",\"owner\":{\"login\":\"palbp\"},\"branches\":[{\"name\":\"DRAG_MoveReviewScreen\",\"commit\":{\"sha\":\"46980481796734442a557de50a750ae3145b86c9\"}},{\"name\":\"DRAG_NewDrawingScreen\",\"commit\":{\"sha\":\"dca41898dd624bd2f2041d21a09ac5cbf73b73bf\"}},{\"name\":\"DRAG_playground\",\"commit\":{\"sha\":\"ed3e3d00dc91bb0357f513fe4f5d8afde64fbd2c\"}},{\"name\":\"DRAG#2_DrawingScreen\",\"commit\":{\"sha\":\"54b95f6221d91e2a197267a880ae180380da037d\"}},{\"name\":\"master\",\"commit\":{\"sha\":\"a13802274b32e90b20c15ffe8d38629b0c84dbd4\"}}],\"fork\":false}]}"))
+	}
+
+	@Test
+	fun getAllUserRepositories_BadRequest_Accept_application_xml() {
+		mock.perform(get("/palbp").header("Accept", "application/xml"))
+			.andDo(print())
+			.andExpect(status().isNotAcceptable)
+	}
+
+	@Test
+	fun getAllUserRepositories_BadRequest_UnknownUser() {
+		mock.perform(get("/userNotKnownAbcdefSupposedToFail").header("Accept", "application/json")) //I'll be really surprised if there is a user with this name
+			.andDo(print())
+			.andExpect(status().isNotFound)
+			.andExpect(content().string("{\"status\":404,\"message\":\"Username does not exist\"}"))
+
+	}
+
+	@Test
+	fun getAllUserRepositories_BadRequest_UsernameHasNoReadableCharacters() {
+		mock.perform(get("/   	").header("Accept", "application/json"))
+			.andDo(print())
+			.andExpect(status().isBadRequest)
+			.andExpect(content().string("{\"status\":400,\"message\":\"Username has no readable characters\"}"))
+	}
+
+//	@Test
+//	fun getAllUserRepositories_BadRequest_
+
 
 }
